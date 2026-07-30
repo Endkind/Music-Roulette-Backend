a60a02331fba39facfadf9deb659388623272661
Endkind <endkind.ender@gmail.com>
2026-07-30 15:44:19 +0200

    introduce bcrypt utility for password hashing and secure token storage in lobby creation

--- routes/v1/lobby.py
+++ routes/v1/lobby.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 
 from fastapi import APIRouter
 
+from core.bcrypt import Bcrypt
 from core.redis import redis_client
 from schemas.response.v1.lobby import *
 
@@ -15,5 +16,5 @@ LOBBY_EXPIRATION = 3600 * 6  # 6 hours
 async def create_lobby():
     uuid = uuid4()
     token = secrets.token_urlsafe(32)
-    await redis_client.set(f"lobby:{uuid}", token, ex=LOBBY_EXPIRATION)
+    await redis_client.set(f"{uuid}", Bcrypt.hash_password(token), ex=LOBBY_EXPIRATION)
     return LobbyCreateResponse(lobby_id=uuid, token=token)
